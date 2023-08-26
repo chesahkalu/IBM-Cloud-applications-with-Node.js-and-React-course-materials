962cc83c815d5cd59e6d23e82e76fc9ed07f111e
chesahkalu <chesahkalu@gmail.com>
2023-08-26 21:42:23 +0100

    all projects

---
+++ CD220Labs/expressjs/node_modules/ibm-cloud-sdk-core/es/lib/content-type.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="node" />
+import { FileObject } from "./helper";
+declare const _default: {
+    fromFilename: (file: String | File | FileObject | NodeJS.ReadableStream | Buffer) => string;
+    fromHeader: (buffer: Buffer) => string;
+};
+export default _default;
---
+++ myexpressapp/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = new express();
+const port = 8080;
+
+app.get("/", (req,res) => {
+    return res.send("Hello World!");
+});
+
+let server = app.listen(port, () => {
+    console.log("listening at http://localhost:"+ 3333)
+})
