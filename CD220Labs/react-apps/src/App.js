897a29a032abb4c0aec41f713197507d55593780
chesahkalu <chesahkalu@gmail.com>
2023-08-27 23:24:45 +0100

    app.js make an api call

--- CD220Labs/react-apps/src/App.js
+++ CD220Labs/react-apps/src/App.js
@@ -11,7 +11,6 @@ export default App;
 */
 
 //Make app.js a functional component
-
 /*
 function App(props) {
   const colorStyle = {
@@ -31,7 +30,6 @@ export default App;
 */
 
 //Make app.js a class component
-
 /*
 import React from 'react';
 
@@ -58,7 +56,8 @@ class App extends React.Component {
 export default App;
 */
 
-//set the props from outside and maintain the state of the component inside. The state we will maintain is the number of times the button was clicked. 
+//set the props from outside and maintain the state of the component inside. The state we will maintain is the number of times the button was clicked.
+/*
 import React from 'react';
 
 class App extends React.Component {
@@ -81,6 +80,54 @@ class App extends React.Component {
         <br /><br />
         {this.state.counter}
       </div>
+    );
+  }
+}
+
+export default App;
+*/
+
+
+//Make an API call to  to a remote server https://api.publicapis.org/entries?category=Animals when the component mounts, and change the state when the remote server responds and display the list of APIs in the browser.
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+  state = { APIlist:[] }
+
+  componentDidMount() {
+    let url = "https://api.publicapis.org/entries?category=Animals";
+    axios({
+      method: 'get',
+      url: url,
+      responseType: 'json'
+    }).then(resp => {
+        let listOfEntries = resp.data.entries;
+        let listOfEntriesAsArray = Object.entries(listOfEntries);
+        let entryDetails = listOfEntriesAsArray.map((entryDetail)=>{
+          return <li style={{color: "green"}}>{entryDetail[1]["API"]}
+          ------- {entryDetail[1]["Link"]} </li>
+        })
+        this.setState({APIlist:<ul>{entryDetails}</ul>})
+      })
+    .catch(err => {
+        console.log(err.toString())
+    });
+  }
+
+  render() {
+    const colorStyle = { color:this.props.color,fontSize:this.props.size+"px"}
+    return (
+      <div style={colorStyle}>
+        <h2>APIs List</h2>
+        <br/>
+
+        <div>
+            {
+            this.state.APIlist
+            }
+        </div>
+    </div>
     );
   }
 }
