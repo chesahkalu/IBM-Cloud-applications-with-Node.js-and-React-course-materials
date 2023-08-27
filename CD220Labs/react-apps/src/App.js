578591cc14e9418fbd8921fc12c01ca9ff6103d9
chesahkalu <chesahkalu@gmail.com>
2023-08-27 23:07:30 +0100

    app.js updates to funtional component in /CD*/react-apps/App.js and styling added in index.js

--- CD220Labs/react-apps/src/App.js
+++ CD220Labs/react-apps/src/App.js
@@ -1,7 +1,26 @@
+/*
 function App() {
   return (
     <div>
       Hello World!
+    </div>
+  );
+}
+
+export default App;
+*/
+
+//Make app.js a functional component
+function App(props) {
+  const colorStyle = {
+    color:props.color,
+    fontSize:props.size+"px"
+  }
+  return (
+    <div>
+      <div style={colorStyle}>
+      Hello World!
+      </div>
     </div>
   );
 }
--- CD220Labs/react-apps/src/index.js
+++ CD220Labs/react-apps/src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App color="red" size="50"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
