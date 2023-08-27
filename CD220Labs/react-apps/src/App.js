55691d1e47fc2ef35129327c62d22303b35bac93
chesahkalu <chesahkalu@gmail.com>
2023-08-27 23:18:13 +0100

    app.js updates to class component in /CD*/react-apps/App.js

--- CD220Labs/react-apps/src/App.js
+++ CD220Labs/react-apps/src/App.js
@@ -11,6 +11,8 @@ export default App;
 */
 
 //Make app.js a functional component
+
+/*
 function App(props) {
   const colorStyle = {
     color:props.color,
@@ -23,6 +25,64 @@ function App(props) {
       </div>
     </div>
   );
+}
+
+export default App;
+*/
+
+//Make app.js a class component
+
+/*
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const colorStyle = {
+    color:this.props.color,
+    fontSize:this.props.size+"px"
+    }
+    return (
+      <div>
+        <div style={colorStyle}>
+        Hello World!
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
+*/
+
+//set the props from outside and maintain the state of the component inside. The state we will maintain is the number of times the button was clicked. 
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  state = { counter:0 }
+
+  incrementCounter = ()=> {
+    this.setState({counter:this.state.counter+1});
+  }
+
+  render() {
+    const colorStyle = { color:this.props.color,fontSize:this.props.size+"px"}
+    return (
+      <div style={colorStyle}>
+        React Component
+        <br /><br />
+        <button onClick={this.incrementCounter}>Click Me!</button>
+        <br /><br />
+        {this.state.counter}
+      </div>
+    );
+  }
 }
 
 export default App;
